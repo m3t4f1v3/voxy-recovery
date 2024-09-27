7a7857b8f6d785ee0c5989e37798a1ed86d67e4b
metafive <40429600+m3t4f1v3@users.noreply.github.com>
2024-09-27 10:38:01 +1000

    awesome

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <cstring>
 #include <iostream>
+#include <fstream>
 #include <rocksdb/db.h>
 #include <rocksdb/options.h>
 #include <stdexcept>
 #include <vector>
 #include <zstd.h>
 #include <bit>
+#include <zlib.h>
+#include <sstream>
+#include <iomanip>
+#include <nbt_tags.h>
 
 struct WorldSection
 {
   int64_t key;
-  std::vector<int64_t> data; // Adjust size based on your section's structure
+  std::vector<int64_t> data;
 };
+
+std::vector<nbt::value> mappings(1 << 20);
 
 class RocksDBHandler
 {
@@ -26,7 +33,6 @@ private:
   rocksdb::DB *db;
   std::vector<rocksdb::ColumnFamilyHandle *> handles;
 
-  std::string decompressZSTD(const std::string &compressed_data);
   bool deserialize(WorldSection &section, const std::string &decompressedData,
                    bool ignoreMismatchPosition);
 
@@ -50,6 +56,7 @@ RocksDBHandler::RocksDBHandler(const std::string &db_path)
 
   rocksdb::Status status = rocksdb::DB::Open(rocksdb::DBOptions(), db_path,
                                              cf_descriptors, &handles, &db);
+  std::cout << "Opening " << handles.size() << " column families" << std::endl;
   if (!status.ok())
   {
     throw std::runtime_error("Error opening RocksDB database: " +
@@ -66,48 +73,9 @@ RocksDBHandler::~RocksDBHandler()
   delete db;
 }
 
-#include <fstream>
-
-void writeBufferToFile(const char *buffer, size_t size,
-                       const std::string &filename)
+std::string decompressZSTD(const std::string &compressed_data)
 {
-  std::ofstream outFile(filename, std::ios::binary);
-  if (!outFile)
-  {
-    throw std::runtime_error("Failed to open file: " + filename);
-  }
-
-  outFile.write(buffer, size);
-  if (!outFile.good())
-  {
-    throw std::runtime_error("Failed to write to file: " + filename);
-  }
-
-  outFile.close();
-}
-
-void writeVectorToFile(const std::vector<int64_t> &data,
-                       const std::string &filename)
-{
-  std::ofstream outFile(filename, std::ios::binary);
-  if (!outFile)
-  {
-    throw std::runtime_error("Failed to open file: " + filename);
-  }
-
-  outFile.write(reinterpret_cast<const char *>(data.data()),
-                data.size() * sizeof(int64_t));
-  if (!outFile.good())
-  {
-    throw std::runtime_error("Failed to write to file: " + filename);
-  }
-
-  outFile.close();
-}
-
-std::string RocksDBHandler::decompressZSTD(const std::string &compressed_data)
-{
-  size_t decompressed_size =
+  std::size_t decompressed_size =
       ZSTD_getFrameContentSize(compressed_data.data(), compressed_data.size());
 
   if (decompressed_size == ZSTD_CONTENTSIZE_ERROR)
@@ -121,7 +89,7 @@ std::string RocksDBHandler::decompressZSTD(const std::string &compressed_data)
   }
 
   std::string decompressed_data(decompressed_size, 0);
-  size_t result =
+  std::size_t result =
       ZSTD_decompress(&decompressed_data[0], decompressed_size,
                       compressed_data.data(), compressed_data.size());
   if (ZSTD_isError(result))
@@ -131,6 +99,53 @@ std::string RocksDBHandler::decompressZSTD(const std::string &compressed_data)
   }
 
   return decompressed_data;
+}
+
+std::string decompressGzip(const rocksdb::Slice &compressed_data)
+{
+  const int CHUNK_SIZE = 4096;
+  z_stream zs;                // zlib stream
+  memset(&zs, 0, sizeof(zs)); // Zero out the zlib stream
+
+  // Initialize the decompression with gzip header handling
+  if (inflateInit2(&zs, 16 + MAX_WBITS) != Z_OK)
+  {
+    throw std::runtime_error("inflateInit2 failed while decompressing.");
+  }
+
+  zs.next_in = reinterpret_cast<Bytef *>(const_cast<char *>(compressed_data.data()));
+  zs.avail_in = compressed_data.size();
+
+  std::string decompressed_data; // String to hold the decompressed data
+  char outBuffer[CHUNK_SIZE];    // Buffer for decompression output
+
+  int ret;
+
+  // Decompression loop
+  do
+  {
+    zs.next_out = reinterpret_cast<Bytef *>(outBuffer);
+    zs.avail_out = CHUNK_SIZE;
+
+    ret = inflate(&zs, Z_NO_FLUSH);
+
+    // Check for errors during decompression
+    if (ret == Z_STREAM_ERROR || ret == Z_DATA_ERROR || ret == Z_MEM_ERROR)
+    {
+      inflateEnd(&zs); // Clean up
+      throw std::runtime_error("Error during zlib decompression.");
+    }
+
+    // Append the output from the buffer to the decompressed string
+    std::size_t have = CHUNK_SIZE - zs.avail_out; // Amount of data produced
+    decompressed_data.append(outBuffer, have);    // Append to the string
+
+  } while (ret != Z_STREAM_END);
+
+  // Clean up
+  inflateEnd(&zs);
+
+  return decompressed_data; // Return the decompressed data as std::string
 }
 
 template <typename T>
@@ -166,9 +181,10 @@ bool RocksDBHandler::deserialize(WorldSection &section,
 
   // Read LUT length
   int32_t lutLen = readFromBuffer<int32_t>(buffer);
+  // std::cout << "lutlen: " << lutLen << std::endl;
   std::vector<int64_t> lut(lutLen);
   // Hash calculation for key
-  hash = key ^ (lutLen * 1293481298141LL);
+  hash = key ^ (lutLen * 1293481298141L);
 
   // Read LUT and compute hash
   for (int32_t i = 0; i < lutLen; ++i)
@@ -189,12 +205,14 @@ bool RocksDBHandler::deserialize(WorldSection &section,
   for (int32_t i = 0; i < section.data.size(); ++i)
   {
     int16_t lutId = readFromBuffer<int16_t>(buffer);
-    section.data[i] = static_cast<int64_t>(lutId);
+    // std::cout << lutId << std::endl;
+    section.data[i] = lut[lutId];
 
     // Continue hashing
     hash *= 1230987149811L;
     hash += 12831;
-    hash ^= (lutId * 1827631L) ^ lut[lutId];
+    hash ^= (lutId * 1827631L) ^ section.data[i];
+    // std::cout << section.data[i] << std::endl;
   }
   // std::cout << key << " " << lutLen << std::endl;
 
@@ -264,13 +282,64 @@ int64_t withLight(int64_t id, int32_t light)
   return (id & ~(0xFFLL << 56)) | (static_cast<int64_t>(light) << 56);
 }
 
+int32_t getIndex(int32_t x, int32_t y, int32_t z)
+{
+  const int32_t M = (1 << 5) - 1; // Mask for the lowest 5 bits
+  if (x < 0 || x > M || y < 0 || y > M || z < 0 || z > M)
+  {
+    throw std::invalid_argument("Out of bounds: " + std::to_string(x) + ", " +
+                                std::to_string(y) + ", " + std::to_string(z));
+  }
+  return ((y & M) << 10) | ((z & M) << 5) | (x & M);
+}
+
+std::tuple<int, int, int> getCoordinates(int index)
+{
+  int M = (1 << 5) - 1; // Same M value as in getIndex
+
+  // Extract x, y, z from the index using bitwise operations
+  int x = index & M;         // Get the last 5 bits
+  int y = (index >> 10) & M; // Get bits 10 to 14
+  int z = (index >> 5) & M;  // Get bits 5 to 9
+
+  // Check bounds for x, y, z
+  if (x < 0 || x > M || y < 0 || y > M || z < 0 || z > M)
+  {
+    throw std::out_of_range("Out of bounds: " + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z));
+  }
+
+  return std::make_tuple(x, y, z);
+}
+
+void writeVectorToFile(const std::vector<long> &vec, const std::string &filename)
+{
+  std::ofstream outFile(filename, std::ios::binary); // Open file in binary mode
+
+  if (!outFile.is_open())
+  {
+    std::cerr << "Error opening file: " << filename << std::endl;
+    return;
+  }
+
+  // Write the size of the vector first (optional, helps in reading later)
+  std::size_t vectorSize = vec.size();
+  outFile.write(reinterpret_cast<const char *>(&vectorSize), sizeof(vectorSize));
+
+  // Write the raw binary data of the vector to the file
+  outFile.write(reinterpret_cast<const char *>(vec.data()), vec.size() * sizeof(long));
+
+  outFile.close(); // Close the file
+}
+
 void RocksDBHandler::processWorldSections()
 {
+  int i = 0;
   rocksdb::Iterator *it_world_sections =
       db->NewIterator(rocksdb::ReadOptions(), handles[1]);
   for (it_world_sections->SeekToFirst(); it_world_sections->Valid();
        it_world_sections->Next())
   {
+    i++;
     rocksdb::Slice key = it_world_sections->key();
     // std::cout << key.ToString(true) << std::endl;
     // exit(-1);
@@ -301,22 +370,60 @@ void RocksDBHandler::processWorldSections()
 
     if (deserialize(section, decompressed_value, false))
     {
-      std::cout << "World Section Deserialized Successfully:" << std::endl;
-      std::cout << "Key: " << bswap_id << std::endl;
+      // std::cout << "World Section Deserialized Successfully:" << std::endl;
+      // std::cout << "Key: " << bswap_id << std::endl;
       // id = __builtin_bswap64(id);
       // id = 0x90000010000170;
-      std::cout << "World " << getLevel(bswap_id) << ": x=" << getX(bswap_id) << ", y=" << getY(bswap_id) << ", z=" << getZ(bswap_id) << std::endl;
-      for (int64_t blockIdMaybe : section.data)
+      // std::cout << "World " << getLevel(bswap_id) << ": x=" << getX(bswap_id) << ", y=" << getY(bswap_id) << ", z=" << getZ(bswap_id) << std::endl;
+      if (getLevel(bswap_id) == 0) // we are only interested in the 0'th LOD
       {
-        // std::cout << blockIdMaybe << std::endl;
-        if (!isAir(blockIdMaybe))
-        { // why use isAir if id is always 0 if its air :skull:
-          std::cout << blockIdMaybe << " is " << getBlockId(blockIdMaybe) << std::endl;
+        for (int x = 0; x < 32; x++)
+        {
+          for (int y = 0; y < 32; y++)
+          {
+            for (int z = 0; z < 32; z++)
+            {
+              // x, y, z, selfBlockId, key
+              // should be 0, 0, 16, 2, 4503599358935040
+              // std::cout << getIndex(0, 1, 16) << std::endl;
+              // std::cout << section.data[getIndex(x, y, z)] << std::endl;
+              // std::cout << mappings[section.data[getIndex(0, 1, 16)]] << std::endl;
+
+              // filter out air
+
+              int64_t bid = getBlockId(section.data[getIndex(x, y, z)]);
+
+              if (bid != 0)
+              {
+                std::cout << "World " << getLevel(bswap_id) << ": x=" << getX(bswap_id) * 32 + x << ", y=" << getY(bswap_id) * 32 + y << ", z=" << getZ(bswap_id) * 32 + z << std::endl;
+                // std::cout << "x: " << x << " y: " << y << " z: " << z << std::endl;
+                // std::cout << bid << std::endl;
+                // std::cout << section.data[getIndex(x, y, z)] << std::endl;
+                std::cout << mappings[bid] << std::endl;
+              }
+            }
+          }
         }
       }
+      // for (int64_t something : section.data)
+      // {
+      //   int32_t blockId = getBlockId(__builtin_bswap64(something));
+      //   std::cout << blockId << std::endl;
+      //   if (blockId != 0)
+      //   {
+      //     // std::cout << blockId << std::endl;
+      //     // std::cout << getBlockId(blockIdMaybe) << std::endl;
+      //     // std::cout << section.data[getIndex(x, y, z)] << std::endl;
+      //     std::cout << mappings[blockId] << std::endl;
+      //   }
+      // }
       // Access section.data here
-      //   writeVectorToFile(section.data, "test.dat");
+      // if (i == 2)
+      // {
+      //   writeVectorToFile(section.data, "/tmp/test.dat");
+
       //   exit(-1);
+      // }
     }
   }
 
@@ -333,54 +440,84 @@ int32_t bytesToInt(const std::string &i)
 
 void RocksDBHandler::readIdMappings()
 {
-  std::cout << "\nReading from id_mappings:" << std::endl;
+  std::cout << "Reading from id_mappings:" << std::endl;
   rocksdb::Iterator *it_id_mappings =
       db->NewIterator(rocksdb::ReadOptions(), handles[2]);
+  // int i = 0;
   for (it_id_mappings->SeekToFirst(); it_id_mappings->Valid();
        it_id_mappings->Next())
   {
+    // i++;
     rocksdb::Slice key = it_id_mappings->key();
     rocksdb::Slice value = it_id_mappings->value();
     // value is an nbt thingo
     // writeBufferToFile(value.data(), value.size(), "skibidi.dat");
     // exit(-1);
 
-    int32_t id;
-    // int32_t modelId;
-    // nbt::io::stream_reader;
-    // std::cout << value.size() << " " << sizeof(int32_t) << std::endl; // = 8
-    id = bytesToInt(key.ToString());
-    // std::memcpy(&id, key.data(), key.size());
-    // int32_t bswap_id = __builtin_bswap32(id);
-
-    // std::memcpy(&modelId, value.data(), value.size());
-    // int32_t bswap_modelId = __builtin_bswap32(modelId);
+    // int32_t id;
+    // std::cout << key.size() << std::endl;
+    // memcpy(&id, key.data(), key.size());
+    // id = __builtin_bswap32(id);
+    int32_t id = bytesToInt(key.ToString());
+    // id = __builtin_bswap32(id);
+    // std::cout << id << std::endl;
+    // doesnt need to be byteswapped because cortex uses some weird ass conversion
+    // id = bytesToInt(key.ToString());
 
     int32_t entryType = id >> 30;
     int32_t blockId = id & ((1 << 30) - 1);
-    if (entryType == 1) // if its a block, 2 when biome, don't really care about biomes atm since we have the seed
+    // std::cout << entryType << std::endl;
+    if (entryType == 1) // if its a block, 2 when biome, don't really care about biomes atm since we have the seed, this is FUCKED up !, it is sometimes -2 for some god awful reason
     {
-      std::cout << "Key: " << blockId << " Value: " << value.ToString(true) << std::endl;
+      // value.ToString
+      // std::cout << decompressGzip(value) << std::endl;
+      std::istringstream is(decompressGzip(value));
+      nbt::io::stream_reader reader(is);
+      auto pair = nbt::io::read_compound(is);
+      if (pair.second->at("id") != nbt::tag_int(blockId))
+      {
+        std::cerr << "Mapping ID doesn't match NBT" << std::endl;
+      }
+      else
+      {
+        // std::cout << id << std::endl;
+        // std::cout << __builtin_bswap32(id) << std::endl;
+        // std::cout << blockId << std::endl;
+        // std::cout << pair.second->at("block_state") << std::endl;
+        mappings[blockId] = pair.second->at("block_state");
+        // std::cout << "Key: " << blockId << std::endl;
+      }
     }
     // std::cout << "Key: " << id << " Value: " << value.ToString(true) << std::endl;
   }
+  // std::cout << i << std::endl;
   delete it_id_mappings;
 }
 
 int main()
 {
+  // int64_t index = getIndex(10, 10, 10);
+  // std::cout << index << std::endl;
+  // std::tuple<int, int, int> coords = getCoordinates(index);
+  // std::cout << std::get<0>(coords) << ", " << std::get<1>(coords) << ", " << std::get<2>(coords) << std::endl;
+  // exit(-1);
   // int64_t id = 0x0010000070000170;
   // id = __builtin_bswap64(id);
+
   // std::cout << "World " << getLevel(id) << ": x=" << getX(id) << ", y=" << getY(id) << ", z=" << getZ(id) << std::endl;
   // SHOULD be 0, 23, 9, 1
   // exit(-1);
   try
   {
     RocksDBHandler dbHandler(
-        // "/home/edward_wong/.local/share/PrismLauncher/instances/voxy testing/minecraft/saves/debug/voxy/4f2f217931c68a362682a81467b1c221/storage/"
-        "../.voxy/saves/89.168.27.174/0ede3c4bcf2e01e7d3e7fc317625ccae/storage/");
-    // dbHandler.processWorldSections();
+        "/home/edward_wong/.local/share/PrismLauncher/instances/voxy testing/minecraft/saves/empty/voxy/5e08f4cd49c6e5fae140ae6a9bc4228f/storage/");
+    // "../.voxy/saves/89.168.27.174/8c3bc415b64e63ed06e5296642cddd71/storage/");
+
     dbHandler.readIdMappings();
+    // std::cout << mappings.size() << std::endl;
+    // exit(-1);
+    dbHandler.processWorldSections();
+    // dbHandler.readIdMappings();
   }
   catch (const std::exception &e)
   {
